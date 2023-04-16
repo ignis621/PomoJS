49d32d42ed15f3556003b2f8d1b60be29b581555
Ignis <44377195+Ignis621@users.noreply.github.com>
2023-04-16 04:04:34 +0200

    actually added js and mobile warnings

--- warnings.js
+++ warnings.js
@@ -1,7 +1,13 @@
+const dismissMobileBtn = document.getElementById('dismiss-mobile-btn');
 const mobileWarning = document.getElementById('mobile-warning');
 const jsWarning = document.getElementById('js-warning');
 
 jsWarning.classList.add('hidden');
 
-const isMobile = navigator.userAgentData.mobile
-mobileWarning.classList.toggle('hidden', !isMobile);
+if(navigator.userAgent.match(/Mobile|Windows Phone|Lumia|Android|webOS|iPhone|iPod|Blackberry|PlayBook|BB10|Opera Mini|\bCrMo\/|Opera Mobi/i)){
+  mobileWarning.classList.remove('hidden');
+}
+
+dismissMobileBtn.addEventListener('click', () => {
+  mobileWarning.classList.add('hidden');
+});
